b50909019f10d5209d2e64ab4fedf85c7b5a0e49
Greg <szabogergely07@gmail.com>
2017-07-08 13:43:57 +0200

    Add hamburger bar to bottom only on mobile view

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,27 @@
+$(document).on('turbolinks:load', function(){
+
+var win = $(this);      // browser window
+var nav = $('.navbar'); // your navigation bar
+
+function switchNavbar() {
+    if (win.width() < 768) { // on mobile
+        nav.removeClass('navbar-fixed-top');
+        nav.addClass('navbar-fixed-bottom');
+        $("body").css("margin-top", "0px");
+
+    } else { // on desktop
+        nav.removeClass('navbar-fixed-bottom');
+        nav.addClass('navbar-fixed-top');
+        $("body").css("margin-bottom", "0px");
+    }
+}
+
+// On first load
+$(function() {
+    switchNavbar();
+});
+
+
+
+});
+
